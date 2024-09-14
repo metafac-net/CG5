e50b119ebd9d6a5131b678e59fa874287b41129d
Lucian <lucian@metafac.net>
2024-09-14 21:01:20 +1000

    added assembly version checks

--- MetaFac.CG5.Parsing.Tests/RegressionChecks.cs
+++ MetaFac.CG5.Parsing.Tests/RegressionChecks.cs
@@ -1,27 +1,24 @@
 ﻿using PublicApiGenerator;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using VerifyXunit;
+using Xunit;
 
-namespace MetaFac.CG5.Expressions.Tests
+namespace MetaFac.CG5.Parsing.Tests
 {
     public class RegressionChecks
     {
         [Fact]
-        public async Task MetaFac_PublicApi_Expressions()
+        public async Task MetaFac_PublicApi_Parsing()
         {
             // act
             var options = new ApiGeneratorOptions()
             {
                 IncludeAssemblyAttributes = false
             };
-            string currentApi = ApiGenerator.GeneratePublicApi(typeof(CG5Parser).Assembly, options);
+            string currentApi = ApiGenerator.GeneratePublicApi(typeof(Combinators).Assembly, options);
 
             // assert
             await Verifier.Verify(currentApi);
         }
-
     }
 }
---
+++ MetaFac.CG5.Parsing.Tests/VersionChecks.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace MetaFac.CG5.Parsing.Tests
+{
+    public class VersionChecks
+    {
+        [Fact]
+        public void VersionCheck()
+        {
+            typeof(Combinators).Assembly.FullName.Should().StartWith("MetaFac.CG5.Parsing, Version=1.0.");
+        }
+    }
+}
